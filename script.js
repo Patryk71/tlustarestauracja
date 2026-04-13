cd318492d2c38bf9752906d44499fd13fd312799
Patryk71 <1i2022Patryk11@gmail.com>
2026-04-13 10:55:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,3 @@ const menu_mobile = document.querySelector(".menu_mobile");
 przycisk.addEventListener('click',function(){
     menu_mobile.classList.toggle("invisible");
 });
-menu_mobile.addEventListener('blur',function(){
-    menu_mobile.classList.add("invisible");
-});
